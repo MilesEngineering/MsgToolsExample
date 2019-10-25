00778fafdec164f14d415e9faa06c74468031dbf
Caitlin McKenna <caitlin.elizamckenna@gmail.com>
2019-10-25 14:22:01 -0600

    golden layout - add in ability to pull in new elements

--- html/layout.js
+++ html/layout.js
@@ -14,18 +14,26 @@ var config = {
                 componentState: { handler: 'msgtools-msgrx' }
             },{
                 type: 'component',
-                componentName: 'msgComponent',
+                componentName: 'test',
                 componentState: { handler: 'msgtools-msgrx' }
             }]
         }]
     }]
 };
 
-
 // Sets a new Golden Layout instance, using config and attaching to the target container
 // config argumnet is required. if no target is provided, golden layout
 // will take over the document.body
-var msgLayout = new window.GoldenLayout(config, $('#layout_container'));
+
+var msgLayout,
+    savedState = localStorage.getItem( 'savedState' ); // check local storage and use saved state if it exists
+
+if( savedState !== null ) {
+    msgLayout = new GoldenLayout( JSON.parse( savedState ), $('#layout_container') );
+} else {
+    msgLayout = new window.GoldenLayout( config, $('#layout_container'));
+}
+
 
 // Registering components for golden layout
 msgLayout.registerComponent( 'msgComponent', function( container, state ){
@@ -34,16 +42,36 @@ msgLayout.registerComponent( 'msgComponent', function( container, state ){
                                 + '"></msgtools-msgselector></div>');
 });
 
+msgLayout.registerComponent( 'test', function( container, state ){
+    container.getElement().html('<div>What what</div>');
+});
+// END registering components
+
+// Store state in local storage
+msgLayout.on( 'stateChanged', function(){
+    var state = JSON.stringify( msgLayout.toConfig() );
+    localStorage.setItem( 'savedState', state );
+    console.log(state);
+});
 
 //initializing our layout
 msgLayout.init();
 
-// var addMenuItem = function( text ) {
-//     var element = $( '<li>' + text + '</li>' );
-//     $( 'nav' ).append( element );
-//
-//     //insertion code will go here
-// };
-//
-// addMenuItem( 'User added component A' );
-// addMenuItem( 'User added component B' );
+// Add Buttons
+var addMenuItem = function( text, handler ) {
+
+    var element = $( '<button>' + text + '</button>' );
+
+    $( '#menuContainer' ).append( element );
+
+    var newItemConfig = {
+        type: 'component',
+        componentName: 'msgComponent',
+        componentState: { handler: handler }
+    };
+
+    msgLayout.createDragSource( element, newItemConfig );
+};
+
+addMenuItem( 'Add a new msgtools-msgrx', 'msgtools-msgrx' );
+addMenuItem( 'Add a new msgtools-msgtx', 'msgtools-msgtx' );
